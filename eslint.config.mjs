4fc20e9e35f6a555b5eccd4c2ce15d75896e9077
HUS NAIN <jphusnain10@gmail.com>
2025-12-20 14:19:58 +0900

    install the mongos module and fix the ESL Lint
    install winston module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "eslint-define-config";
+import nextPlugin from '@next/eslint-plugin-next';
 
 const eslintConfig = defineConfig({
+  plugins: {
+    next: nextPlugin
+  },
   extends: ["next", "next/core-web-vitals"],
   ignorePatterns: [
     ".next/**",
